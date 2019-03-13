7982724315e921d51c6e02e96a048da28765116c
ledutm2010 <45878959+ledutm2010@users.noreply.github.com>
2019-03-13 17:21:30 -0300

    alteraçoes e novas implemetaçoes

--- eclipse-workspace/PokerGame/src/ComparaCartasJogadores.java
+++ eclipse-workspace/PokerGame/src/ComparaCartasJogadores.java
@@ -1,6 +1,15 @@
+import java.util.List;
+
 //Compara as cartas dos jogadores e define quem é o 
 //vencedor com base nas regras do poker.
 public class ComparaCartasJogadores {
 	
+	//recebe a lista de jogadores e retorna o vencedor de acordo com as regras propostas dentro da funçao 
+	//obs: em implementaçao
+	public Jogador AvaliaCartasJgadores(List<Jogador> lista){
+		Jogador vencedor = new Jogador(); 
+		
+		return vencedor;
+	}
 
 }
--- eclipse-workspace/PokerGame/src/Partida.java
+++ eclipse-workspace/PokerGame/src/Partida.java
@@ -8,37 +8,63 @@ private int totalJogadoresNaPartida = 0;
 
 List<Jogador> jogadoresNaPartida = new ArrayList<Jogador>();
 
-	//adiciona jogadores na partida, respeitando o limite
-	public void AdicionarJogadores(Jogador jogador) {
+	//adiciona jogadores na partida, se ainda tiver vaga, e se ele tiver mais de 20 em cash, que é o valor da aposta inicial
+	public List<Jogador> EntrarNaPartida(Jogador jogador) {
 
-		if (totalJogadoresNaPartida < jogadoresPermitidos) {
+		if (totalJogadoresNaPartida < jogadoresPermitidos && jogador.getCash() > 20) {
 			jogadoresNaPartida.add(jogador);
 			totalJogadoresNaPartida++;
 		}
+		return jogadoresNaPartida;
 	}
 
 
-	//so recebe cartas quem ainda tem cash
-	public void DistribuirCartas(Jogador jogador) {		
+	//faz a aposta minima e distribui as cartas aos jogadores
+	public List<Jogador> IniciarPartida(List<Jogador> jogadoresNaPartida) {		
 		
 		//cria o baralho randomico
-		DeckCartas deckcartas = new DeckCartas();
-		deckcartas.GerarDeckCartas();
+		DeckCartas deckCartas = new DeckCartas();
+		deckCartas.GerarDeckCartas();
 		
-		//distribuir as cartas
-		for (int i = 0; i < totalJogadoresNaPartida; i++) {
+		//cada jogador da partida recebe 5 cartas, e paga 20 (minimo) para jogar
+		for (int i = 0; i < jogadoresNaPartida.size(); i++) {
 			//pega o jogador na lista na partida
 			Jogador jog = new Jogador();
 			jog = jogadoresNaPartida.get(i);
 			
+			//subtrair o valor da aposta para montar a premiaçao
+			MontarPremiacao(jog);
+			
 			//dar 5 cartas para cada jogador
 			for (int j = 0; j < 5; j++) {
 				Carta carta = new Carta();
-				carta = deckcartas.GerarDeckCartas().get(j);
+				carta = deckCartas.GerarDeckCartas().get(j);
 				jog.CartasJogador(carta);
 			}	
 		}
+		return jogadoresNaPartida;
 	}
 
+	//montar a premiaçao
+	public int MontarPremiacao(Jogador jogador){
+		int apostaTotal = 0;
+		//pega o jogador na lista na partida e subtrair o valor da aposta minima, e credita o valor subtraido no premio
+		jogador.setCash(-20);
+		apostaTotal = apostaTotal + 20;	
+		
+		return apostaTotal;
+	}
 	
+	//declara o vencedor da partida e credita o premio 
+	//obs: em implementaçao
+	public Jogador ApontarVencedor(List<Jogador> jogadoresNaPartida){
+		int premio;
+		//premio = MontarPremicao(jogador);
+		Jogador vencedor = new Jogador();
+		ComparaCartasJogadores comparaCartas = new ComparaCartasJogadores();
+		vencedor = comparaCartas.AvaliaCartasJgadores(jogadoresNaPartida);
+		//vencedor.setCash(+premio);
+		return vencedor;
+		
+	}
 }
